fd8b857e88149c3cecbf1f4f2eff7931d7f5070c
unknown <khan.farhan1985@gmail.com>
2023-10-20 09:02:29 +0530

    Initial Commit

---
+++ src/main/java/com/example/dockerdemofinalexample/DockerDemoFinalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerdemofinalexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoFinalExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoFinalExampleApplication.class, args);
+	}
+
+}
