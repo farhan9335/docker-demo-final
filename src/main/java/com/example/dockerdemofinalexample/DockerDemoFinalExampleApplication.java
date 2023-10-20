925ffae6912e7e7a869638c32666efdc4cab0280
LENOVO <khan.farhan1985@gmail.com>
2023-10-20 09:11:46 +0530

    [Added Dockerfile]

--- src/main/java/com/example/dockerdemofinalexample/DockerDemoFinalExampleApplication.java
+++ src/main/java/com/example/dockerdemofinalexample/DockerDemoFinalExampleApplication.java
@@ -2,12 +2,20 @@ package com.example.dockerdemofinalexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerDemoFinalExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoFinalExampleApplication.class, args);
+	}
+	
+	@GetMapping(path = "/welcome")
+	public String getMessage() {
+		return "Final Example";
 	}
 
 }
